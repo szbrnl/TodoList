d01de7e4bbb4bdaa55c96860e1759f5510f134eb
Szymon Bernal <szymon.bernal@gmail.com>
2017-09-30 19:59:05 +0200

    mongo

---
+++ TodoList/Syncing/TaskListSync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+
+namespace TodoList.Syncing
+{
+    public class TaskListSync
+    {
+        
+    }
+}
